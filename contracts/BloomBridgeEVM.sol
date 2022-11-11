e5413d270ffec39a455fecc172cb371d8f4ba5db
Alex Fiorenza <alexfiorenza2012@gmail.com>
2022-11-11 17:43:50 -0300

    Testing of wormhole bridge messages

--- contracts/BloomBridgeEVM.sol
+++ contracts/BloomBridgeEVM.sol
@@ -1,27 +1,49 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface CallProxy {
-    function callProxy(address _target, bytes calldata _data)
-        external
-        returns (bytes memory);
-}
+pragma solidity 0.8.0;
+import "./Wormhole/IWormhole.sol";
 
 contract BloomBridgeEVM {
-    address private constant CALL_PROXY_ADDRESS =
-        0xD2b88BA56891d43fB7c108F23FE6f92FEbD32045;
-    address private destination;
+    string private current_msg;
     address private DAI;
-    address private RECEIVER;
-    string private RECEIVER_CHAIN;
+    address wormhole_core_bridge_address =
+        address(0x706abc4E45D419950511e474C7B9Ed348A4a716c);
+    uint32 nonce = 0;
+    address owner;
+    mapping(uint16 => bytes32) _applicationContracts;
+    IWormhole core_bridge = IWormhole(wormhole_core_bridge_address);
+    mapping(bytes32 => bool) _completedMessages;
 
-    constructor(
-        address _dai,
-        address receiverAddress,
-        string memory receiver_chain
-    ) {
-        DAI = _dai;
-        RECEIVER = receiverAddress;
-        RECEIVER_CHAIN = receiver_chain;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function sendMsg(bytes memory str) public returns (uint64 sequence) {
+        sequence = core_bridge.publishMessage(nonce, str, 200);
+        nonce = nonce + 1;
+    }
+
+    function receiveEncodedMsg(bytes memory encodedMsg) public {
+        (IWormhole.VM memory vm, bool valid, string memory reason) = core_bridge
+            .parseAndVerifyVM(encodedMsg);
+        require(valid, reason);
+        require(
+            _applicationContracts[vm.emitterChainId] == vm.emitterAddress,
+            "Invalid Emitter Address!"
+        );
+        require(!_completedMessages[vm.hash], "Message already processed");
+        _completedMessages[vm.hash] = true;
+        current_msg = string(vm.payload);
+    }
+
+    function getCurrentMsg() public view returns (string memory) {
+        return current_msg;
+    }
+
+    function registerApplicationContracts(
+        uint16 chainId,
+        bytes32 applicationAddr
+    ) public {
+        require(msg.sender == owner, "Only owner can register new chains!");
+        _applicationContracts[chainId] = applicationAddr;
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,8 +6,7 @@ async function main() {
     'BloomBridgeEVM'
   );
   const currentNetworkId = network.config.chainId;
-  const { _dai } = getContractsAddresses(network.config.chainId as any);
-  // const bloomBridgeEVM = await BloomBridgeEVMFactory.deploy(_dai);
+  const bloomBridgeEVM = await BloomBridgeEVMFactory.deploy();
   // await bloomBridgeEVM.deployed();
   // signale.success(
   //   `Treasure contract was deployed to:${bloomBridgeEVM.address} 🚀🚀 `
@@ -41,40 +40,6 @@ async function verify(contractAddress: string, args: any[]) {
     } else {
       console.log(error);
     }
-  }
-}
-function getContractsAddresses(chainId: 5 | 1 | 97 | 56) {
-  switch (chainId) {
-    case 5:
-      // goerli testnet
-      return {
-        _dai: '0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844',
-        _usdc: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
-        _usdt: '0x2DB274b9E5946855B83e9Eac5aA6Dcf2c68a95F3',
-      };
-    case 1:
-      // ethereum mainnet
-      return {
-        _dai: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-        _usdc: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-        _usdt: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-      };
-    case 97:
-      // bnb testnet
-      return {
-        _dai: '0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867',
-        _usdc: '0x64544969ed7EBf5f083679233325356EbE738930',
-        _usdt: '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd',
-      };
-    case 56:
-      // bnb mainnet
-      return {
-        _dai: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
-        _usdc: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
-        _usdt: '0x55d398326f99059fF775485246999027B3197955',
-      };
-    default:
-      throw new Error('Invalid chainId');
   }
 }
 
