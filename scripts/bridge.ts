3b410fa571c1d351928d2cd16b4634fcb05c94fa
Alex Fiorenza <alexfiorenza2012@gmail.com>
2022-11-16 16:52:26 -0300

    Have to fix invalid emmiter

--- scripts/bridge.ts
+++ scripts/bridge.ts
@@ -2,6 +2,9 @@ import {
   getEmitterAddressEth,
   parseSequenceFromLogEth,
   tryNativeToHexString,
+  CONTRACTS,
+  ChainId,
+  coalesceChainName,
 } from '@certusone/wormhole-sdk';
 import { ethers } from 'hardhat';
 import signale from 'signale';
@@ -9,37 +12,49 @@ import signale from 'signale';
 async function main() {
   const bridgeAmt = ethers.utils.parseUnits('3', 18);
   const Bridge = await ethers.getContractFactory('BloomBridgeEVM');
+  signale.pending('Bridging your token...');
   const bloomBridge = await Bridge.attach(
     '0xc6D2C9a921bFc451161971352bC6B00840103A64' // TODO: Replace with your deployed contract address
   );
   const targetRecepient = Buffer.from(
     tryNativeToHexString(
-      '0xc6D2C9a921bFc451161971352bC6B00840103A64', // The address of the user that want to be bridge
-      'ethereum'
+      '0xF274800E82717D38d2e2ffe18A4C6489a50C5Add', // The address of the user that want to be bridge
+      'polygon'
     ),
     'hex'
   );
   const tx = await (
-    await bloomBridge.bridgeDAIToMumbai(bridgeAmt, 8001, targetRecepient)
+    await bloomBridge.bridgeDAIToMumbai(bridgeAmt, 5, targetRecepient)
   ).wait();
   signale.success(`Goerli bridge done tx hash: ${tx.transactionHash}`);
   const emmiterAddr = getEmitterAddressEth(
-    '0xB4DC0c495a4304cA549B17f17D86298207D05E48' // Wormhole bridge contract address
+    getTokenBridgeAddressForChain(2) // Wormhole token bridge contract address
   );
-  console.log(emmiterAddr);
-  console.log(tx.logs);
-  const seq = parseSequenceFromLogEth(tx, emmiterAddr);
-  const vaaURL = `https://wormhole-v2-testnet-api.certus.one
-  /v1/signed_vaa/2/${emmiterAddr}/${seq}`;
+  const seq = parseSequenceFromLogEth(
+    tx,
+    '0x706abc4E45D419950511e474C7B9Ed348A4a716c' //Wormhole core bridge address
+  );
+  const vaaURL = `https://wormhole-v2-testnet-api.certus.one/v1/signed_vaa/2/${emmiterAddr}/${seq}`;
   console.log(vaaURL);
   signale.pending('Waiting for VAAs');
   let vaaBytes = await (await fetch(vaaURL)).json();
+  signale.await(
+    'Grab a coffee while we wait for the VAA this is going to be a long one... ☕☕ '
+  );
   while (!vaaBytes.vaaBytes) {
-    signale.error('VAA not found, retrying in 5s!');
     await new Promise((r) => setTimeout(r, 5000)); //Timeout to let Guardiand pick up log and have VAA ready
     vaaBytes = await (await fetch(vaaURL)).json();
   }
+  signale.success('VAA found!');
+  signale.pending('Completing transfer');
+  const completeTransfer = await (
+    await bloomBridge.completeTransfer(Buffer.from(vaaBytes.vaaBytes, 'base64'))
+  ).wait();
+  console.log(completeTransfer);
 }
+
+const getTokenBridgeAddressForChain = (chainId: ChainId) =>
+  CONTRACTS['TESTNET'][coalesceChainName(chainId)].token_bridge || '';
 
 main().catch((error) => {
   console.error(error);
